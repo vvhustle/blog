cf0f2bc047f5e59206c4b9a081298a47e55ec285
wonhee <wonhee3795@naver.com>
2020-07-23 17:52:17 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# leveloper.dev
+# delivan.dev
+POWERED BY [Gastby starter bee](https://github.com/JaeYeopHan/gatsby-starter-bee).
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/8fc0562f-201e-43b8-b806-2940de5b2d49/deploy-status)](https://app.netlify.com/sites/hyuk/deploys)
---
+++ gatsby-meta-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  title: `leveloper.dev`,
+  description: `Blog posted about ...`,
+  author: `[Your Name]`,
+  introduction: `그저 공부한 것과 한 일을 기록한 노션의 백업.`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `leveloper97`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: `wh_space`,
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [
+    `blog`,
+    `book-review`
+  ],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  // sponsor: {
+  //   buyMeACoffeeId: 'jbee',
+  // },
+  share: {
+    facebookAppId: '100002406117066', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
