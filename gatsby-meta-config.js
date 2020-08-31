29c41bec511af1cbfcc0cf6c5edfcee71431488e
JWH <leveloper97@gmail.com>
2020-08-31 16:29:42 +0900

    favicon 추가

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -11,7 +11,7 @@ module.exports = {
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/favicon.png`, // Add your favicon
   keywords: [
     `blog`,
     `book-review`
